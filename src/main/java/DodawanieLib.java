a09eddc325136e71f77df20a840814de9c844964
piotr6789 <43881310+piotr6789@users.noreply.github.com>
2018-11-27 21:59:35 +0100

    Update DodawanieLib.java

--- src/main/java/DodawanieLib.java
+++ src/main/java/DodawanieLib.java
@@ -14,7 +14,7 @@ public class DodawanieLib {
 
         int ai = Integer.parseInt(a);
         int bi = Integer.parseInt(b);
-        if(ai <= 100001 && bi <= 1000) {
+        if(ai <= 1000001 && bi <= 1000001) {
             return String.valueOf(ai+bi);
         }
         else if(a.length() == 5)
